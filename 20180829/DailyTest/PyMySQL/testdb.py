c3620be859c55b4dc06507ec350a1c0aa84784c6
FrozenMap <ljjdbd123@hotmail.com>
2018-08-29 11:23:21 +0000

    add [initdb]

--- 20180829/DailyTest/PyMySQL/testdb.py
+++ 20180829/DailyTest/PyMySQL/testdb.py
@@ -3,7 +3,7 @@ import mysql.connector, logging, datetime
 def doSQL(sql, args=''):
 	res = None
 	try:
-		conn = mysql.connector.connect(user='jjayyyyyyy', password='', database='cos')
+		conn = mysql.connector.connect(user='xxx', password='yyy', database='zzz')
 		cursor = conn.cursor()
 		cursor.execute(sql)
 		if args == 'retrieve':
@@ -18,13 +18,21 @@ def doSQL(sql, args=''):
 
 	return res
 
+def initdb():
+	sql = 'drop table if exists menu'
+	doSQL(sql)
+	sql = 'create table menu (menu_id int primary key not null, menu_name varchar(40) not null, menu_date date not null);'
+	doSQL(sql)
+	sql = "insert into menu values(1, 'hello', '20180825')"
+	doSQL(sql)
+
 # CRUD
 
 def create():
 	print('\n---------before insert:---------')
 	retrieve()
 
-	sql = "insert into menu values (6, 'zzz', '20180535')"
+	sql = "insert into menu values (2, 'zzz', '20180827')"
 	doSQL(sql);
 
 	print('\n---------after insert:---------')
@@ -43,7 +51,7 @@ def update():
 	print('\n---------before update:---------')
 	retrieve()
 
-	sql = "update menu set menu_date = '20180829' where menu_id = 6"
+	sql = "update menu set menu_date = '20180829' where menu_id = 2"
 	doSQL(sql)
 
 	print('\n---------after update:---------')
@@ -54,7 +62,7 @@ def delete():
 	print('\n---------before delete:---------')
 	retrieve()
 
-	sql = "delete from menu where menu_id = 6"
+	sql = "delete from menu where menu_id = 2"
 	doSQL(sql)
 	
 	print('\n---------after delete:---------')
@@ -63,6 +71,8 @@ def delete():
 	print('\n\n')
 
 if __name__ == '__main__':
+	initdb()
+
 	create()
 
 	update()
